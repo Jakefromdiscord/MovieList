f36a520208a92c1e63580a2cd336cc3e51569051
Jacob <jacobwamsley12@gmail.com>
2022-01-19 20:29:12 -0700

    Movie list intermediate complete

--- index.js
+++ index.js
@@ -26,16 +26,27 @@ function addMovie(event){
 function deleteMovie(event){
     event.target.parentNode.remove()
     message.textContent = 'Movie Deleted!'
+    message.textContent = event.target.parentNode.firstChild.textContent + " " + 'Deleted!'
+
+    revealMessage()
 }
 
 function crossOffMovie(event){
     event.target.classList.toggle('checked')
     if(event.target.classList.contains('checked')){
-        message.textContent = "Moive Watched"
+        message.textContent = event.target.textContent + ' ' + 'watched!'
     } else{
-        message.textContent = "Movie added back"
+        message.textContent = event.target.textContent + ' ' + 'added back!'
     }
     
+    revealMessage()
+}
+
+function revealMessage(){
+    message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000);
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
